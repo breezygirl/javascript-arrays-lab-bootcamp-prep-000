0cc0ad429dce78f12dcabb78e34e744a006a4ddb
Beerscheba C <designergo@aol.com>
2018-06-15 01:36:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const app = "I don't do much."
+kittens=["Milo", "Otis", "Garfield"]
+function destructivelyAppendKitten(){
+   var destructivelyAppendKitten = kittens.push("Ralph");
+   return destructivelyAppendKitten
+}
+function destructivelyPrependKitten(){
+ var  destructivelyPrependKitten = kittens.unshift("Bob")
+  return kittens
+}
+function destructivelyRemoveLastKitten(){
+  
+  var destructivelyRemoveLastKitten=  
+  kittens.pop()
+  return ["Milo", "Otis"]
+}
+function destructivelyRemoveFirstKitten(){
+  var destructivelyRemoveFirstKitten = kittens.shift()
+  return ["Otis", "Garfield"]
+  }
+  
+  function appendKitten(){
+   var appendKitten = kittens.concat("Broom")
+   
+    return appendKitten
+  }
+  
+  function prependKitten(){
+  
+   var prependKitten = ["Arnold", ...kittens]
+   return prependKitten
+    }
+    
+    function removeLastKitten(){
+      
+     var removeLastKitten= kittens.slice()
+      return removeLastKitten
+    }
