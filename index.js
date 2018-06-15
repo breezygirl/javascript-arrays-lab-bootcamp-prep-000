57d4040296fa5f202e08c5ac799f68250e1b0ea9
Beerscheba C <designergo@aol.com>
2018-06-15 01:55:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,5 +37,6 @@ function destructivelyRemoveFirstKitten(){
       }
     function removeLastKitten(){
       
-      var removeLastKitten. kitens.slice()
+      var removeLastKitten. kitens.slice(0, -1)
+      return removeLastKitten
     }
